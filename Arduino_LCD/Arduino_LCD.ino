29f187f13fdc9572fed75abfd8803beaa234c428
Fatemeh <aboulhassani.fatemeh@gmail.com>
2024-07-01 22:51:21 +0330

    lcd-show a word

---
+++ Arduino_LCD/Arduino_LCD.ino
@@ -0,0 +1,11 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);// LiquidCrystal lcd(rs, enable, D4, D5, D6, D7)
+
+void setup() {
+lcd.begin(16, 2); //(column, row)
+lcd.print("Fatemeh");
+}
+
+void loop() {
+}
